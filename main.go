1d0d72ca7d243f2e77e0087304f6db706c4c81e3
taye000 <taylorgitari@gmail.com>
2023-03-07 17:13:40 +0300

    variables

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
-func main ()  {
-	name := "taye"
+func main() {
+	var nameTwo string = "Tgod"
+	var ageOne int = 23
+	var nameThree = "Tee"
+	var ageTwo = 23
+	nameOne := "taye"
+	ageThree := 23
 
-	fmt.Println(name)
+	fmt.Println(nameOne, ageThree, nameTwo, ageOne, nameThree, ageTwo)
 }
