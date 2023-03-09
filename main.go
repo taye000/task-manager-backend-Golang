6a688c72abbdeb50dde53f9104e5c03104423ab9
taye000 <taylorgitari@gmail.com>
2023-03-09 13:41:37 +0300

    sort

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-// Importing fmt package to format & print to the console
-import "fmt"
+/*Importing fmt packages
+fmt is used to print to the console
+strings is used to work with strings*/
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
 func main() {
 	//initializing variables with var keyword and type
-	var nameTwo string = "Tgod"
+	var nameTwo string = "Tgod major"
 	var ageOne int = 23
 
 	//initializing variables with var keyword and inferred type
@@ -36,12 +42,38 @@ func main() {
 	var sliceOne = []int{9, 8, 4, 3}
 	//update a value from the slice
 	sliceOne[0] = 20
-	/*append a value to the slice 
+	/*append a value to the slice
 	append returns a new slice with the appended value
 	It does not update the slice, unless you assign the new returned
 	slice to be the updated slice as below*/
 	sliceOne = append(sliceOne, 5)
 
+	//slice ranges, sliceOne[startIndexIncluding:upToNotIncluding]
+	rangeOne := sliceOne[0:2]
+	//sliceOne[1:] means from index 1 to the end of the slice
+	rangeTwo := sliceOne[1:]
+	//sliceOne[:2] means from the start of the slice to index 2 not including index 2
+	rangeThree := sliceOne[:2]
+
+	//updating nameTwo variable with the new string returned from the ReplaceAll method
+	nameTwo = strings.ReplaceAll(nameTwo, "major", "minor")
+
+	//using sort package to sort the slice. Sort is in-place, meaning it updates the original slice
+	sort.Ints(sliceOne)
+	fmt.Println("sliceOne:", sliceOne)
+
+	//using strings package to print to the console
+	fmt.Println(strings.Contains(nameTwo, "major"))
+	/*using strings package Replace method to replace a string with another string
+	It returns a new string, Not updating the original*/
+	fmt.Println(strings.ReplaceAll(nameTwo, "major", "minor"))
+	//using strings.toUpper method to convert a string to uppercase
+	fmt.Println(strings.ToUpper(nameTwo))
+	//using the strings.Split method to split a string into a slice
+	fmt.Println(strings.Split(nameTwo, " "))
+
+	//printing out the ranges of the slices
+	fmt.Println(rangeOne, rangeTwo, rangeThree)
 	fmt.Println(arrOne, arrTwo, arrThree, len(arrOne))
 
 	//printing variables to the console using fmt.Println
